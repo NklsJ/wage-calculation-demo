74b7947f7618deb5371d9d51b078d013d2d1a4a0
Niklas Juhonen <niklasjuhonen@hotmail.com>
2019-04-25 19:34:17 +0300

    Refactor types

--- src/wages/types.js
+++ src/wages/types.js
@@ -1,11 +1,29 @@
 // @flow
 
-type PersonWageData = {
-  id: Number,
-  fullname: String,
-  monthlyWage: String,
+type ShiftObject = {
+  start: string,
+  end: string,
 }
 
-type MonthlyWages = Array<PersonWageData>
+type DayObjects = {
+  date: string,
+  hours: number,
+  eveninigHours: number,
+  overtimeHours: number,
+  wage: number,
+  shifts: Array<ShiftObject>,
+}
 
-export type WageData = Array<MonthlyWages>
+export type Worker = {
+  id: number,
+  fullname: string,
+  days: Array<DayObjects>
+}
+
+export type MonthObject = {
+  month: number,
+  year: number,
+  workers: Array<Worker>,
+}
+
+export type MonthlyWages = Array<MonthObject>
